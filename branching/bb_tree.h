4c10dd3c7217a3962b7c3d936a6ea6c26a7761e1
Alberto Santini <santini.alberto@gmail.com>
2013-09-29 21:44:46 +0200

    "Empty box" for the bb_tree

---
+++ branching/bb_tree.h
@@ -0,0 +1,11 @@
+//  Created by Alberto Santini on 18/09/13.
+//  Copyright (c) 2013 Alberto Santini. All rights reserved.
+//
+
+#ifndef BB_TREE_H
+#define BB_TREE_H
+
+#include <base/base.h>
+#include <branching/bb_node.h>
+
+#endif
