fac67caea4e839977b5565336841dc0f742df911
Alberto Santini <santini.alberto@gmail.com>
2013-12-04 21:35:50 +0100

    Further step in graph preprocessing

--- base/problem.cpp
+++ base/problem.cpp
@@ -10,7 +10,7 @@ Problem::Problem(string params_file, string data_file) {
     graphs = GraphMap();
     
     for(std::shared_ptr<VesselClass> vessel_class : data.vessel_classes) {
-        std::shared_ptr<Graph> g = GraphGenerator::create_graph(data, vessel_class);
+        std::shared_ptr<Graph> g = GraphGenerator::create_graph(data, vessel_class, params.remove_additional_arcs);
         cerr << "Graph for " << vessel_class->name << ": " << num_vertices(g->graph) << " vertices and " << num_edges(g->graph) << " edges" << endl;
         graphs.emplace(vessel_class, g);
     }
