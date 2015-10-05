ce93132264552ee047ba89f6eabc5c5296430e9e
Alberto Santini <santini.alberto@gmail.com>
2015-10-05 14:42:18 +0200

    Added launcher script and related data

---
+++ opt/launch_jobs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cmdline_args=($*)
+base_dir="/home/alberto/or-bologna/maritime-vrp/data"
+
+for file in $(printf "%s%s%s%s" "${base_dir}" "/new/" "${cmdline_args[0]}" "*.json")
+do
+  base_name=$(basename $file)
+  grep "${base_name}" solved_instances.txt
+  return_code=$?
+  
+  if [[ "${return_code}" == "1" ]]
+  then
+    echo "Launching job for ${file}"
+    oarsub -n "${base_name}" -O "${base_name}.out" -E "${base_name}.err" --p "network_address!='drbl10-201-201-21'" -p "memcore='3'" -l /nodes=1/core=2,walltime=5 "../build/maritime_vrp ${base_dir}/old_thesis_data/program_params.json ${base_dir}/new/${base_name}"
+  else
+    echo "Skipping job for ${file}"
+  fi
+done
