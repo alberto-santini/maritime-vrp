55784e7b63587c7792790fcdc3d11bdabce5d0da
Alberto Santini <santini.alberto@gmail.com>
2015-10-09 09:31:54 +0200

    Reorganised the launch script slightly

--- opt/launch_jobs.sh
+++ opt/launch_jobs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 cmdline_args=($*)
-base_dir="/home/alberto/or-bologna/maritime-vrp/data"
+base_dir="/home/alberto/or-bologna/maritime-vrp"
 
-for file in $(printf "%s%s%s%s" "${base_dir}" "/new/" "${cmdline_args[0]}" "*.json")
+for file in $(printf "%s%s%s%s" "${base_dir}" "/data/new/" "${cmdline_args[0]}" "*.json")
 do
   base_name=$(basename $file)
   grep "${base_name}" solved_instances.txt
@@ -12,7 +12,7 @@ do
   if [[ "${return_code}" == "1" ]]
   then
     echo "Launching job for ${file}"
-    oarsub -n "${base_name}" -O "${base_name}.out" -E "${base_name}.err" --p "network_address!='drbl10-201-201-21'" -l /nodes=1/core=2,walltime=5 "${base_dir}/../opt/timeout -m 3145728 ${base_dir}/..build/maritime_vrp ${base_dir}/old_thesis_data/program_params.json ${base_dir}/new/${base_name}"
+    oarsub -n "${base_name}" -O "${base_name}.out" -E "${base_name}.err" --p "network_address!='drbl10-201-201-21'" -l /nodes=1/core=2,walltime=5 "${base_dir}/opt/timeout -m 3145728 ${base_dir}/build/maritime_vrp ${base_dir}/data/old_thesis_data/program_params.json ${base_dir}/data/new/${base_name}"
   else
     echo "Skipping job for ${file}"
   fi
