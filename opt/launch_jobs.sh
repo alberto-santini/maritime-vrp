747646786a5f096aaf23c777fdfbd56a3217c116
alberto-santini <santini.alberto@gmail.com>
2016-10-13 10:02:56 +0200

    Modified launcher script for unibo cluster

--- opt/launch_jobs.sh
+++ opt/launch_jobs.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 cmdline_args=($*)
-base_dir="/home/alberto/or-bologna/maritime-vrp"
+base_dir="/home/alberto/src/maritime-vrp"
 
 for file in $(printf "%s%s%s%s" "${base_dir}" "/data/new/" "${cmdline_args[0]}" "*.json")
 do
   base_name=$(basename $file)
   grep "${base_name}" solved_instances.txt
   return_code=$?
-  
+
   if [[ "${return_code}" == "1" ]]
   then
     echo "Launching job for ${file}"
-    oarsub -n "${base_name}" -O "${base_name}.out" -E "${base_name}.err" --p "network_address!='drbl10-201-201-21'" -l /nodes=1/core=2,walltime=5 "${base_dir}/opt/timeout -m 3145728 ${base_dir}/build/maritime_vrp ${base_dir}/data/program_params.json ${base_dir}/data/new/${base_name}"
+    oarsub -n "${base_name}" -O "${base_name}.out" -E "${base_name}.err" --p "network_address!='drbl10-201-201-21'" -l /nodes=1/core=2,walltime=5 "LD_LIBRARY_PATH=~/local/lib64 ${base_dir}/build/maritime_vrp ${base_dir}/data/program_params.json ${base_dir}/data/new/${base_name}"
   else
     echo "Skipping job for ${file}"
   fi
